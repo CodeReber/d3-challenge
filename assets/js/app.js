d5b940a6e98b23824c65c00e91b7effdb256fafd
codereber <jamesmpyle@gmail.com>
2020-10-10 15:04:49 -0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,4 +12,13 @@ var margin = {
 var height = svgHeight = margin.top - margin.bottom;
 var width = svgWidth = margin.right - margin.left;
 
-var svg = d3.select("#scatter")
+var svg = d3
+    .select("scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
